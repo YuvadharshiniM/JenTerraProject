d0cc55b9bdfd55ef9d037a8086bd28caab705152
YuvadharshiniM <yuvadharshinim05@gmail.com>
2026-02-14 11:34:05 +0530

    Enhance variable definitions with description
    Added descriptions for AWS variables and updated ami_id default.

--- variables.tf
+++ variables.tf
@@ -1,24 +1,30 @@
 variable "aws_region" {
-  type    = string
-  default = "ue-north-1"
+  description = "AWS region where resources will be created"
+  type        = string
+  default     = "ue-north-1"
 }
-variable "instance_type" {
-  type    = string
-  default = "t3.micro"
-}
-variable "ami_id" {
-  type = string
 
-  validation {
-    condition     = length(trimspace(var.ami_id)) > 0
-    error_message = "ami-0b6c6ebed2801a5cb"
-  }
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
 }
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instance (Ubuntu for us-east-1)"
+  type        = string
+  default     = "ami-073130f74f5ffb161"
+}
+
 variable "existing_key_pair_name" {
-  type    = string
-  default = "terraform"
+  description = "Existing AWS key pair name"
+  type        = string
+  default     = "terraform"
 }
+
 variable "allowed_ssh_cidr" {
-  type    = string
-  default = "0.0.0.0/0"
+  description = "CIDR block allowed to SSH into EC2"
+  type        = string
+  default     = "0.0.0.0/0"
 }
+
